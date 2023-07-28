da1811ea89f668f419602d5b50c1c091f41942bd
eyad <eshneiker@gmail.com>
2023-07-27 23:51:25 -0500

    All Solved

---
+++ problems/09-chain-map.js
@@ -0,0 +1,58 @@
+/*******************************************************************************
+Write a function `chainMap` that accepts a value and any number of callbacks as
+arguments. The function should return the final result of passing the value through
+all of the given callbacks. In other words, if three callbacks are given then:
+
+- the value is given to the first callback
+- the result of the first callback is given to the second callback
+- the result of the second callback is given to the third callback
+- the result of the third callback is the final result
+
+Examples:
+*******************************************************************************/
+debugger
+
+let chainMap = function(num, cb1, cb2, cb3, cb4) {
+    let result = num;
+    if ( cb1 !== undefined ){
+        result = cb1 (result);
+    }
+    if ( cb2 !== undefined ){
+        result = cb2(result);
+    }
+    if ( cb3 !== undefined ){
+        result = cb3 (result);
+    }
+    if ( cb4 !== undefined ){
+        result = cb4 (result);
+    }
+
+    return result;
+};
+
+let add5 = function(n) {
+    return n + 5;
+};
+
+let half = function(n) {
+    return n / 2;
+};
+
+let square = function(n) {
+    return n * n;
+};
+
+console.log(chainMap(25, add5));                // 30
+console.log(chainMap(25, add5, half));          // 15
+console.log(chainMap(25, add5, half, square));  // 225
+console.log(chainMap(4, square, half));         // 8
+console.log(chainMap(4, half, square));         // 4
+
+
+
+
+
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = chainMap;
---
+++ problems/16-suffix-cipher.js
@@ -0,0 +1,63 @@
+/*******************************************************************************
+Write a function `suffixCipher` that accepts a sentence and object as arguments.
+The object contains suffixes as keys and callbacks as values. The `suffixCipher`
+function should return a new sentence where words of the original sentence are
+modified according to the callback that corresponds with the suffix that the word
+ends with. If the word does not end in any of the suffix keys, then it should not
+be modified. You can assume that only one suffix of the object will match a word.
+
+Examples:
+
+*******************************************************************************/
+debugger
+
+let suffixCipher = function(sentence, obj) {
+    let words = sentence.split(" ");
+    let newWords = [];
+    for ( let i = 0; i < words.length; i++){
+        let suffixFound = false;
+
+        for ( let key in obj){
+            let value = obj[key];
+            if ( words[i].endsWith(key)){
+                newWords.push(value(words[i]));
+                suffixFound = true;
+            }
+        }
+
+        if ( suffixFound === false ){
+            newWords.push(words[i]);
+        }
+    }
+    return newWords.join(" ");
+}
+
+let cipher1 = {
+    ly: function(word) {
+        return word.slice(0, -1) + 'ee';
+    },
+    ize: function(word) {
+        return word + 'r';
+    }
+};
+console.log(suffixCipher('quietly and gently visualize', cipher1));
+// quietlee and gentlee visualizer
+
+let cipher2 = {
+    tal: function(word) {
+        return word.toUpperCase();
+    },
+    s: function(word) {
+        return word + 'th';
+    }
+};
+console.log(suffixCipher('incremental progress is very instrumental', cipher2));
+// INCREMENTAL progressth isth very INSTRUMENTAL
+
+
+
+
+
+
+/*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
+module.exports = suffixCipher;
